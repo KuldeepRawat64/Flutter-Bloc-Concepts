cc1875799566e70e401c0c3d88dbefce9e4e5af2
KuldeepRawat64 <kuldeeprwt64@gmail.com>
2022-11-01 13:32:38 +0530

    Annonyms routes with Bloc access

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'cubit/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -74,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     FloatingActionButton(
+                      heroTag: 'FA1',
                       onPressed: () {
                         BlocProvider.of<CounterCubit>(context).decrement();
                       },
@@ -81,6 +86,95 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Icon(Icons.remove),
                     ),
                     FloatingActionButton(
+                      heroTag: 'FA2',
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).increment();
+                      },
+                      tooltip: 'Increment',
+                      child: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => BlocProvider.value(
+                                    value:
+                                        BlocProvider.of<CounterCubit>(context),
+                                    child: const NewScreen(title: 'New Screen'),
+                                  )));
+                    },
+                    child: const Text('Go to Second screen'))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewScreen extends StatefulWidget {
+  final String title;
+  const NewScreen({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<NewScreen> createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Column(
+              children: [
+                BlocConsumer<CounterCubit, CounterState>(
+                  listener: (context, state) {
+                    if (state.isIncremented == true) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          duration: Duration(milliseconds: 100),
+                          content: Text('Incremented!')));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          duration: Duration(milliseconds: 100),
+                          content: Text('Decremented!')));
+                    }
+                  },
+                  builder: (context, state) {
+                    return Text(
+                      '${state.counterValue}',
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    FloatingActionButton(
+                      heroTag: 'FA3',
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decrement();
+                      },
+                      tooltip: 'Decrement',
+                      child: const Icon(Icons.remove),
+                    ),
+                    FloatingActionButton(
+                      heroTag: 'FA4',
                       onPressed: () {
                         BlocProvider.of<CounterCubit>(context).increment();
                       },
